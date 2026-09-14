4aeb59ca84665ef7fa7846b1c880d012b759246f
iswaryalaxmi19-ui <iswaryalaxmi19@gmail.com>
2026-09-14 21:35:24 +0530

    addded solution

--- Linear Search.py
+++ Linear Search.py
@@ -1,1 +1,12 @@
+arr = [2,5,20,12,24]
+target = 12
+
+def linearSearch(arr,target):
+    for i in range(len(arr)):
+     if arr[i]==target:
+        return i
+    print("target not found")
+
+result=linearSearch(arr,target)
+print(result)
 
