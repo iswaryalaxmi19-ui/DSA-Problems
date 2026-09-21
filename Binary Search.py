c4374c60016ffdc4c2908302e4b2676484ec71ea
iswaryalaxmi19-ui <iswaryalaxmi19@gmail.com>
2026-09-22 00:38:39 +0530

    Update binary search

--- Binary Search.py
+++ Binary Search.py
@@ -1,1 +1,13 @@
-
+def binary_search(arr,target):
+    left=0
+    right=len(arr) -1
+    while left<=right:
+        middle=(left+right)//2
+        if arr[middle] == target:
+            return middle;
+        elif arr[middle] < target:
+            left = middle +1
+        elif arr[middle] > target:
+            right = middle -1
+    return -1;
+        
