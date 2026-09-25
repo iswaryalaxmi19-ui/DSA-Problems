e57d9d17b4e7e306ed56613bcc0c08b30af41e17
iswaryalaxmi19-ui <iswaryalaxmi19@gmail.com>
2026-09-25 21:29:49 +0530

    Solution added

---
+++ Reverse array.py
@@ -0,0 +1,8 @@
+def reverse(arr):
+    left = 0
+    right = len(arr) - 1
+    while left < right:
+        arr[left],arr[right] = arr[right],arr[left]
+        left = left +1
+        right =  right -1
+    return arr
